ef197d245d93dde8fb32a48c1440ae8fc7bbd284
Genilton Barbosa <genilton.barbosa@boxti.com.br>
2022-09-23 14:56:12 -0300

    first commit

---
+++ chat-server.py
@@ -0,0 +1,52 @@
+import socket
+from _thread import *
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+IP_address = "10.255.0.164"
+Port = 65432
+
+server.bind((IP_address, Port))
+
+server.listen(100)
+
+list_of_clients = []
+
+def clientthread(conn, addr):
+	conn.send("Bem vindo!\n".encode('UTF-8'))
+
+	while True:
+			try:
+				message = conn.recv(2048)
+				if message:
+					print ("<" + addr[0] + "> " + message)
+					message_to_send = "<" + addr[0] + "> " + message
+					broadcast(message_to_send, conn)
+
+				else:
+					remove(conn)
+
+			except:
+				continue
+
+def broadcast(message, connection):
+	for clients in list_of_clients:
+		if clients!=connection:
+			try:
+				clients.send(message)
+			except:
+				clients.close()
+				remove(clients)
+
+def remove(connection):
+	if connection in list_of_clients:
+		list_of_clients.remove(connection)
+
+while True:
+	conn, addr = server.accept()
+	list_of_clients.append(conn)
+
+	print (addr[0] + " ta ON!")
+	start_new_thread(clientthread,(conn,addr))
+
---
+++ chat.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+from _thread import *
+from threading import Thread
+
+def receive_message(IP_address, Port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((IP_address, Port))
+
+    while True:
+        message = server.recv(2048)
+        sys.stdout.write(message.decode('UTF-8'))
+
+def send_message(IP_address, Port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((IP_address, Port))
+
+    while True:
+        message = sys.stdin.readline()
+        server.send(message.encode('UTF-8'))
+        # sys.stdout.write("<Vc> ")
+        # sys.stdout.write(message)
+        sys.stdout.flush()
+
+IP_address = "10.255.0.164"
+Port = 65432
+
+
+
+receive_thread = Thread(target=receive_message,args=(IP_address, Port))
+send_thread = Thread(target=send_message,args=(IP_address, Port))
+
+receive_thread.start()
+send_thread.start()
+
+receive_thread.join()
+send_thread.join()
+
